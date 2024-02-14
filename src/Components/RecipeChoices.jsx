0a9da9748468b04de149ee5d2c614f7aeb3db4c5
Hamza <103555231+Hamzoozz@users.noreply.github.com>
2024-02-14 00:08:39 -0500

    Update RecipeChoices.jsx

--- src/Components/RecipeChoices.jsx
+++ src/Components/RecipeChoices.jsx
@@ -22,3 +22,31 @@ const RecipeChoices = ({ handleChange, label, choices, currentVal }) => {
 };
 
 export default RecipeChoices;
+
+
+/* Answer Choice Version
+import React, { Component, useEffect, useState } from "react";
+
+const RecipeChoices = ({ handleChange, label, choices, checked }) => {
+    return (
+        <div className="radio-buttons">
+        {choices &&
+          choices.map((choice) => (
+            <li key={choice}>
+              <input
+                id={choice}
+                value={choice}
+                name={label}
+                type="radio"
+                onChange={handleChange}
+                checked={checked == choice}
+              />
+                {choice}
+            </li>
+          ))}
+      </div>
+    );
+};
+
+export default RecipeChoices;
+*/
